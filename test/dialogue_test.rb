064531f1829368357511068231a02d7bd61b3b63
amcolling <amcolling@gmail.com>
2018-05-18 22:07:39 -0600

    created run_dialogue loop for dialogue output in the game

--- test/dialogue_test.rb
+++ test/dialogue_test.rb
@@ -1,5 +1,14 @@
-require '.test/test_helper.rb'
 require './lib/dialogue.rb'
+require './test/test_helper.rb'
 
 class DialogueTest < Minitest::Test
+  def test_it_exists
+    dialogue = Dialogue.new
+    assert_instance_of Dialogue, dialogue
+  end
+
+  def test_run_dialogue
+    dialogue = Dialogue.new
+    assert_instance_of String, dialogue.run_dialogue
+  end
 end
