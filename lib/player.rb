77ad63fe97f6e9024900d7c71928c75a107fa051
amcolling <amcolling@gmail.com>
2018-05-23 18:11:04 -0600

    working on converting user input

--- lib/player.rb
+++ lib/player.rb
@@ -1,18 +1,13 @@
 class Player
-  attr_reader :guess
+  attr_reader :user_input
 
-  def initialize
-    @guess = gets.chomp
+  def initialize(user_input)
+    @user_input = user_input
   end
 
-
-
-
-
-
-
-
-
-
+  def convert_user_input_to_an_array
+    converted_input =  @user_input.split(" ")
+    converted_input
+  end
 
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -5,12 +5,22 @@ require './lib/player.rb'
 
 class PlayerTest < Minitest::Test
   def test_it_exists
-  player = Player.new
+  player = Player.new("B1, B2")
+  dialogue = Dialogue.new
   assert_instance_of Player, player
   end
 
   def test_player_has_a_guess
-    player = Player.new
-    assert_instance_of String, player.guess
+    player = Player.new("B1, B2")
+    dialogue = Dialogue.new
+    assert_instance_of String, player.user_input
   end
+
+  def test_convert_user_input_to_a_string
+    player = Player.new("B1, B2")
+    dialogue = Dialogue.new
+    assert_equal "", player.convert_user_input_to_an_array
+  end
+
+
 end
